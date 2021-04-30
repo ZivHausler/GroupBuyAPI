a3a534c4722695c4599d7c1f857a8022be45ed9b
danielezraa <80195225+danielezraa@users.noreply.github.com>
2021-04-30 16:27:03 +0300

    fixed CORS

--- server.js
+++ server.js
@@ -56,9 +56,9 @@ app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`)
 })
 
-// CORS:
-// app.use(function (req, res, next){
-//     res.header("Access-Control-Allow-Origin","*");
-//     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+//CORS:
+app.use(function (req, res, next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
