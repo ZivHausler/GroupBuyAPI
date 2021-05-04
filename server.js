c9cf30430ca3f2c70eed810dd81e3e506254433f
danielezraa <80195225+danielezraa@users.noreply.github.com>
2021-05-04 19:20:47 +0300

    trying to commit

--- server.js
+++ server.js
@@ -58,8 +58,9 @@ app.listen(process.env.PORT || 3000, () => {
 })
 
 //CORS:
-app.use(function (req, res, next){
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
+// app.use(function (req, res, next){
+//     res.header("Access-Control-Allow-Origin","*");
+//     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
